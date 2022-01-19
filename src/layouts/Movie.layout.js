a7b697d344a288cbd7c6e098bc5ed9779fd768ce
Shivansh770 <shivanshgupta770@gmail.com>
2022-01-19 21:29:45 +0530

    fwaltu

---
+++ src/layouts/Movie.layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MovieNavbar from "../components/Navbar/movieNavbar.component";
+const MovieLayout = (props)  => {
+   return(
+      <div>
+      <MovieNavbar />
+      {props.children}
+      </div>
+   );
+};
+
+export default MovieLayout;
